2da535eaf66251aa22eaf5027602b54df3fd9471
atomforevertoxic <139710109+atomforevertoxic@users.noreply.github.com>
2025-02-22 00:42:52 +0300

    добавлен поворот ГГ по движению мыши

--- Assets/__Scripts/Player/PlayerMovement.cs
+++ Assets/__Scripts/Player/PlayerMovement.cs
@@ -1,28 +1,39 @@
 using UnityEngine;
+using UnityEngine.UIElements;
 using static UnityEditor.Searcher.SearcherWindow.Alignment;
 
 [RequireComponent(typeof(Rigidbody))]
 public class PlayerMovement : MonoBehaviour
 {
+
+    [Tooltip("ћаска дл€ всего, во что может целитьс€ игрок")]
+    [SerializeField] private LayerMask _groundMask;
+
+
+
     [SerializeField] public float _speed = 0.3f;
 
     private Rigidbody _rb;
+    private Camera _cam;
 
+    private Vector3 _mousePoint;
     public Vector3 LastMovedVector {  get; private set; }
     public Vector3 MovementVector { get; private set; }
 
     private void Start()
     {
         _rb = GetComponent<Rigidbody>();
-
         _rb.constraints = RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezeRotationZ;
 
-        LastMovedVector = Vector3.right;
+        _cam = Camera.main;
+
+        LastMovedVector = transform.forward;
     }
 
     private void Update()
     {
         InputLogic();
+        Aim();
     }
     private void FixedUpdate()
     {
@@ -36,24 +47,36 @@ public class PlayerMovement : MonoBehaviour
         float verticalInput = Input.GetAxis("Vertical");
 
         MovementVector = new Vector3(horizontalInput, 0.0f, verticalInput).normalized;
-
-        SetLastMovedVector();
     }
 
-
-    private void SetLastMovedVector()
+    private void Aim()
     {
-        if (MovementVector.x!=0 || MovementVector.z!=0)
+        if (GetMousePosition())
         {
-            LastMovedVector = new Vector3(MovementVector.x, 0, MovementVector.z);
+            var direction = _mousePoint - transform.position;
+
+            direction.y = 0;
+
+            transform.forward = direction.normalized;
+
+            LastMovedVector = transform.forward;
         }
     }
 
+    private bool GetMousePosition()
+    {
+        var ray = _cam.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray, out var hitInfo, Mathf.Infinity, _groundMask))
+        {
+            _mousePoint = hitInfo.point;
+            return true;
+        }
+        return false;
+    }
 
     private void MoveLogic()
     {
         _rb.AddForce(MovementVector * _speed);
-        transform.rotation = Quaternion.LookRotation(LastMovedVector);
     }
 
 
--- Assets/__Scripts/Weapon/Active/ActiveWeaponController.cs
+++ Assets/__Scripts/Weapon/Active/ActiveWeaponController.cs
@@ -1,56 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class ActiveWeaponController : WeaponController
 {
-
-    [SerializeField] private LayerMask _groundMask;
-
-    private Vector3 _mousePoint;
-
     public Vector3 ShootDirection { get; private set; }
 
-
-    private Camera _cam;
     protected override void Start()
     {
         base.Start();
-        _cam = Camera.main;
-
     }
     private void Update()
     {
-        Aim();
-    }
-
-    private void Aim()
-    {
-        if (Input.GetMouseButtonDown(0) && GetMousePosition())
+        if (Input.GetMouseButtonDown(0))
         {
-            var direction = _mousePoint - transform.position;
-
-            direction.y = 0;
-            
-            ShootDirection = direction.normalized;
-
             Attack();
         }
     }
 
-    private bool GetMousePosition()
-    {
-        var ray = _cam.ScreenPointToRay(Input.mousePosition);
-        if (Physics.Raycast(ray, out var hitInfo, Mathf.Infinity, _groundMask))
-        {
-            _mousePoint = hitInfo.point;
-            return true;
-        }
-        return false;
-    }
 
     protected override void Attack()
     {
+        ShootDirection = transform.forward;
         GameObject go = Instantiate(_weapon);
         go.transform.position = transform.position;
         go.transform.rotation = Quaternion.identity;
