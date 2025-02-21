89fc27c2d2ee2aadf5af25c0ff235ff949f6e871
SergeyGT <79414642+SergeyGT@users.noreply.github.com>
2025-02-21 15:12:42 +0300

    Написан скрипт, отвечающий за логику повышения уровня и сбор XP

--- Assets/__Scripts/UI/XPBar.cs
+++ Assets/__Scripts/UI/XPBar.cs
@@ -1,18 +1,53 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class XPBar : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [Header("Experience")]
+    [SerializeField] AnimationCurve curve;
+
+    private int currentLevel;
+    private int totalXP;
+    private int previousLevelXP;
+    private int nextLevelXP;
+
+    [Header("Interface")]
+    [SerializeField] private TextMeshProUGUI levelText;
+    [SerializeField] private Image fill;
+
+    public void AddExp(int amount)
     {
-        
+        totalXP += amount;
+        CheckForLevelUp();
+        UpdateInterface();
+    }
+    private void CheckForLevelUp()
+    {
+        if (levelText != null)
+        {
+            if(totalXP >= nextLevelXP)
+            {
+                currentLevel++;
+                UpdateLevel();
+            }
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void UpdateLevel()
     {
-        
+        previousLevelXP = (int)curve.Evaluate(currentLevel);
+        nextLevelXP = (int)curve.Evaluate(currentLevel + 1);
+        UpdateInterface();
+    }
+
+    private void UpdateInterface()
+    {
+        int start = totalXP - previousLevelXP;
+        int end = nextLevelXP - previousLevelXP;
+        levelText.text = currentLevel.ToString();
+        fill.fillAmount = (float)start/(float)end;
     }
 }
