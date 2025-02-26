1ecc9391cc3c17f504d5ae2313cc5a41efd598f9
atomforevertoxic <139710109+atomforevertoxic@users.noreply.github.com>
2025-02-26 22:42:21 +0300

    KnifeBehaviour fix

--- Assets/__Scripts/Weapon/Passive/Weapon Behaviours/KnifeBehaviour.cs
+++ Assets/__Scripts/Weapon/Passive/Weapon Behaviours/KnifeBehaviour.cs
@@ -1,3 +1,4 @@
+using DG.Tweening;
 using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
@@ -14,12 +15,12 @@ public class KnifeBehaviour : ProjectileWeaponBehaviour
 
         //transform.position = _knifeController.transform.position;
         Direction = _knifeController.PlayerMovement.LastMovedVector.normalized * Speed;
+        transform.rotation = Quaternion.LookRotation(Direction);
     }
 
     protected void Update()
     {
         transform.position += Direction * Time.deltaTime;
-        transform.rotation = Quaternion.LookRotation(Direction);
     }
 
     protected override void OnTriggerEnter(Collider other)
