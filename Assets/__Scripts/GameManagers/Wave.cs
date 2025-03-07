20793791d5ab981bdaa98f2eb6623413b7a80302
SergeyGT <79414642+SergeyGT@users.noreply.github.com>
2025-03-07 21:40:20 +0300

    Update Wave.cs

--- Assets/__Scripts/GameManagers/Wave.cs
+++ Assets/__Scripts/GameManagers/Wave.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Wave
+public class Wave : MonoBehaviour
 {
     [SerializeField] private AnimationCurve _skeleton;
     [SerializeField] private AnimationCurve _shooter;
