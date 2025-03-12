b06b5e93d09ce66fcbfc273eeae8868fb0fada58
ShatalovIvan <139710109+atomforevertoxic@users.noreply.github.com>
2025-03-13 01:47:39 +0300

    Реализована пауза

--- Assets/__Scripts/GameManagers/GameManager.cs
+++ Assets/__Scripts/GameManagers/GameManager.cs
@@ -2,17 +2,99 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+//Finite-State Machine Manager
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    [SerializeField] private GameState _currentState;
+    private GameState _previousState;
+
+    [Header("UI")]
+    [SerializeField]private GameObject _pauseScreen;
+
+    //Состояния игры
+    private enum GameState
     {
-        
+        Gameplay,
+        Paused,
+        GameOver
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Awake()
     {
-        
+        DisableScreens();
     }
+
+    private void Update()
+    {
+        switch(_currentState)
+        {
+            case GameState.Gameplay:
+                CheckForPauseAndResume();
+                break;
+            case GameState.Paused:
+                CheckForPauseAndResume();
+                break;
+            case GameState.GameOver:
+                ///
+                break;
+            default:
+                Debug.LogWarning("Недопустимое игровое состояние! " + _currentState);
+                break;
+        }
+    }
+
+    private void CheckForPauseAndResume()
+    {
+        Debug.Log("Check");
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Debug.Log("Esc pressed");
+            if (_currentState==GameState.Paused)
+            {
+                Debug.Log("Resume");
+                ResumeGame();
+            }
+            else
+            {
+                PauseGame();
+            }
+        }
+    }
+
+    public void PauseGame()
+    {
+        if (_currentState!=GameState.Paused)
+        {
+            _previousState = _currentState;
+            ChangeState(GameState.Paused);
+            Time.timeScale = 0f; //Остановка игры
+            _pauseScreen.SetActive(true);
+            Debug.Log("Игра встала на паузу");
+        }
+    }
+
+    private void ChangeState(GameState state)
+    {
+        _currentState = state;
+    }
+
+    public void ResumeGame()
+    {
+        if (_currentState == GameState.Paused)
+        {
+            ChangeState(_previousState);
+            Time.timeScale = 1f; //Возобновление игры
+            _pauseScreen.SetActive(false);
+            Debug.Log("Игра вышла из паузы");
+        }
+    }
+
+
+    private void DisableScreens()
+    {
+        _pauseScreen.SetActive(false);
+    }
+
 }
