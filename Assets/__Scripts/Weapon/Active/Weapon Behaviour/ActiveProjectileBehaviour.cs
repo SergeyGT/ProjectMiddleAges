02943c0a6003693f407f71b3f4609dffd4aca874
Shatalov Ivan <139710109+atomforevertoxic@users.noreply.github.com>
2025-03-19 01:26:38 +0300

    Добавлены лук и стрелы для активного оружия

--- Assets/__Scripts/Weapon/Active/Weapon Behaviour/ActiveProjectileBehaviour.cs
+++ Assets/__Scripts/Weapon/Active/Weapon Behaviour/ActiveProjectileBehaviour.cs
@@ -5,9 +5,20 @@ using UnityEngine;
 
 public class ActiveProjectileBehaviour : ProjectileWeaponBehaviour
 {
+    private void OnEnable()
+    {
+        //”станавливаю горизонтальное положение стрелы
+        Vector3 currentRotation = transform.rotation.eulerAngles;
+
+        currentRotation.x -= 90; 
+
+        transform.rotation = Quaternion.Euler(currentRotation);
+    }
+
+
     protected void Update()
     {
-        transform.position += transform.forward * Time.deltaTime * weaponData.Speed;
+        transform.position -= transform.up * Time.deltaTime * weaponData.Speed;
     }
 
     protected override void OnTriggerEnter(Collider other)
