d800c1959a2efeccbc4f29800a90b77a1e9c1c27
Shatalov Ivan <139710109+atomforevertoxic@users.noreply.github.com>
2025-03-18 14:39:21 +0300

    {BugFix] Исправлено случайное наведение топоров

--- Assets/__Scripts/Weapon/Passive/Weapon Behaviours/AxeBehaviour.cs
+++ Assets/__Scripts/Weapon/Passive/Weapon Behaviours/AxeBehaviour.cs
@@ -6,17 +6,22 @@ public class AxeBehaviour : MeleeWeaponBehaviour
 
     //По семантике методов AxeBehaviour больше подходит для классов ближнего боя
 
-    [SerializeField] private float Max_X_Range = 10;
+    [SerializeField] private float Max_X_Range = 5;
     
-    [SerializeField] private float max_Z_Range = 10;
+    [SerializeField] private float max_Z_Range = 5;
     
-    [SerializeField] private float TOP_Y_VALUE = 20;
+    [SerializeField] private float TOP_Y_VALUE = 5;
 
     private float UNDER_GROUND_VALUE = -5;
 
+    private void OnEnable()
+    {
+        
+    }
+
     public override void MakeAttack()
     {
-        Vector3 fallSpot = new Vector3(Random.Range(-Max_X_Range, Max_X_Range), TOP_Y_VALUE, Random.Range(-max_Z_Range, max_Z_Range));
+        Vector3 fallSpot = transform.position + new Vector3(Random.Range(-Max_X_Range, Max_X_Range), TOP_Y_VALUE, Random.Range(-max_Z_Range, max_Z_Range));
 
         float goingUpDuration = weaponData.Duration * 0.3f;
 
